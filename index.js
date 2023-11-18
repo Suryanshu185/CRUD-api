6044925361c7076648090194b1d08fd5792908bf
Suryanshu Gupta <91721569+Suryanshu185@users.noreply.github.com>
2023-11-18 19:17:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for all origins
 app.use(cors());
